d26d173a98baba2ac7f6d911204453bf47850736
Eliza <rileydatlord@gmail.com>
2022-07-27 18:35:28 -0700

    Command cooldowns (#1)
    * Implement command cooldowns
    
    * Ignore .idea folder
    
    * purely to test push thing
    
    * Format document with prettier

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,13 @@ import 'reflect-metadata';
 
 import { importx } from '@discordx/importer';
 import { Client } from 'discordx';
+import NodeCache from 'node-cache';
+import { CommandInteraction } from 'discord.js';
+import { createEmbed } from './utils/embed';
 
 require('dotenv').config();
+
+const commandCache = new NodeCache({ stdTTL: 2.5 });
 
 export const client = new Client({
 	intents: [],
@@ -18,12 +23,24 @@ client.on('ready', async () => {
 });
 
 client.on('interactionCreate', (interaction) => {
+	if (interaction instanceof CommandInteraction) {
+		let name = interaction.commandName;
+		if (commandCache.has(name)) {
+			interaction.reply({
+				ephemeral: true,
+				embeds: [createEmbed('⏱ This command was used recently, please wait')]
+			});
+			return;
+		}
+		commandCache.set(name, true);
+	}
+
 	client.executeInteraction(interaction);
 });
 
 async function start() {
 	await importx(__dirname + '/commands/*.{js,ts}');
-	client.login(process.env.TOKEN!!);
+	await client.login(process.env.TOKEN!!);
 }
 
 start();
