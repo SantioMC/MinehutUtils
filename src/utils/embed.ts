65b366d81afa41360f4fa456a7b3d078ceb53537
Santio <SantioTheBest123@gmail.com>
2022-09-07 19:10:30 -0400

    Fix invalid last online times

--- src/utils/embed.ts
+++ src/utils/embed.ts
@@ -8,7 +8,7 @@ export function createEmbed(description: string): EmbedBuilder {
 }
 
 export function toEmbed(server: ServerData): EmbedBuilder {
-	const startTime = Math.floor(server.last_online / 1000);
+	var startTime: number | null = Math.floor(server.last_online / 1000);
 	const creationDate = Math.floor(server.creation / 1000);
 
 	const maxPlayers = server.proxy ? '∞' : server.maxPlayers || 10;
@@ -17,6 +17,9 @@ export function toEmbed(server: ServerData): EmbedBuilder {
 		`\n📈 **Players:** ${server.playerCount}/${maxPlayers}` +
 		`\n📆 **Created:** <t:${creationDate}:R>` +
 		`\n📁 **Categories:** ${server.categories.length == 0 ? 'None' : server.categories.join(', ')}`;
+
+	if (!startTime || isNaN(startTime) || new Date(startTime).getTime() == -1)
+		startTime = creationDate;
 
 	return createEmbed(
 		(server.suspended ? `:warning: This server is currently suspended!\n` : '') + description
@@ -27,7 +30,9 @@ export function toEmbed(server: ServerData): EmbedBuilder {
 			{
 				name: 'Server Status',
 				value:
-					`Server is \`${server.online ? 'online' : 'offline'}\` ${server.online ? '✅' : '❌'}` +
+					`Server is \`${server.online ? 'online' : 'offline'}\` ${
+						server.online ? '<:yes:659939181056753665>' : '<:no:659939343875702859>'
+					}` +
 					`\n${server.online ? `Started On:` : `Last Online:`} <t:${startTime}:R>` +
 					`\nCreated At: <t:${creationDate}:R>`,
 				inline: true
