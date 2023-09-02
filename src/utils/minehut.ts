e12de47d61a7d034cdc9b8ca170bdc097ad93b8d
SantioMC <business@santio.me>
2023-09-02 16:10:26 -0400

    fix: gradients & color codes in motd

--- src/utils/minehut.ts
+++ src/utils/minehut.ts
@@ -24,13 +24,13 @@ export function cleanMOTD(motd: string): string {
 		.replace(/```/g, '') // Clean off code blocks
 		.replace(/\n /g, '\n') // Fix newline spacing
 		.replace(/^\s+/g, '') // Get rid of leading whitespace
-		.replace(/§[0-9a-frmno]/g, '')
+		.replace(/[§&][0-9a-frmno]/g, '')
 		.replace(
 			/<\/?(black|dark_blue|dark_green|dark_aqua|dark_red|dark_purple|gold|gray|dark_gray|blue|green|aqua|red|light_purple|yellow|white|rainbow)>/gi,
 			''
 		) // Remove all color tags
 		.replace(/<\/?((?:color:)?#[0-9A-F]{6})>/gi, '') // Remove all hex color tags
-		.replace(/<\/?((?:gradient|transition):(?:#[0-9A-F]{6})+)>/gi, '') // Remove all gradient tags
+		.replace(/<\/?((?:gradient|transition)(?::#?[a-z0-9]+)+)>/gi, '') // Remove all gradient tags
 		.replace(
 			/<\/?(bold|b|italic|em|i|underlined|u|strikethrough|st|obfuscated|obf|reset|color|transition|rainbow|newline)>/gi,
 			''
