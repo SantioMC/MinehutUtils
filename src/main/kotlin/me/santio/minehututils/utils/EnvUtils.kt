3ea23483779dd8bf28675b4aa7971bc5192b632e
SantioMC <business@santio.me>
2023-11-04 18:12:27 -0400

    feat: Add compatibility for real environment variables

--- src/main/kotlin/me/santio/minehututils/utils/EnvUtils.kt
+++ src/main/kotlin/me/santio/minehututils/utils/EnvUtils.kt
@@ -5,19 +5,20 @@ import io.github.cdimascio.dotenv.Dotenv
 
 @Suppress("unused")
 object EnvUtils {
-    private val dotenv = Dotenv.load()
+    private val dotenv = Dotenv.configure().ignoreIfMissing().load()
 
     fun env(key: String, def: String? = null): String {
-        return dotenv[key.uppercase()] ?: def ?: throw IllegalStateException("No environment variable for '$key'")
+        return env(key) ?: def ?: throw IllegalStateException("Missing environment variable $key")
     }
 
     @Suppress("UNCHECKED_CAST")
     fun <T: Any> env(key: String, def: T): T {
-        return dotenv[key.uppercase()] as? T ?: def
+        return env(key) as? T ?: def
     }
 
     fun env(key: String): String? {
         return dotenv[key.uppercase()]
+            ?: System.getenv(key.uppercase())
     }
 
 }
