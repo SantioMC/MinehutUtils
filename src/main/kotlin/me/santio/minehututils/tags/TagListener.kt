32e16574b78fed6d325b580d1d7c471b2730020e
SantioMC <admin@santio.me>
2024-12-11 01:22:02 -0500

    feat: tag cooldown

--- src/main/kotlin/me/santio/minehututils/tags/TagListener.kt
+++ src/main/kotlin/me/santio/minehututils/tags/TagListener.kt
@@ -1,9 +1,16 @@
 package me.santio.minehututils.tags
 
+import net.dv8tion.jda.api.entities.emoji.Emoji
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 object TagListener: ListenerAdapter() {
+
+    private val cooldownReaction = Emoji.fromUnicode("⌛")
+    private val executor = Executors.newSingleThreadScheduledExecutor()
+    private val recentlySent = mutableListOf<String>()
 
     override fun onMessageReceived(event: MessageReceivedEvent) {
         val tags = TagManager.all()
@@ -11,7 +18,18 @@ object TagListener: ListenerAdapter() {
         val tag = tags.firstOrNull { it.isIncluded(event.message.contentRaw) }
             ?: return
 
+        val id = "${event.channel.id}-${tag.id}"
+        if (recentlySent.contains(id)) {
+            event.message.addReaction(cooldownReaction).queue()
+            return
+        }
+
+        recentlySent.add(id)
         tag.send(event.message)
+
+        executor.schedule({
+            recentlySent.remove(id)
+        }, 10, TimeUnit.SECONDS)
     }
 
 }
