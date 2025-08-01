5a185a5aebd912690e6d3eb9434b1b3774cf34fa
tarna <46247728+tarna@users.noreply.github.com>
2025-08-01 17:47:43 -0400

    refactor: change to data class

--- src/main/kotlin/me/santio/minehututils/database/models/GuildData.kt
+++ src/main/kotlin/me/santio/minehututils/database/models/GuildData.kt
@@ -4,7 +4,7 @@ import gg.ingot.iron.annotations.Model
 import gg.ingot.iron.strategies.NamingStrategy
 
 @Model(table = "guild_data", naming = NamingStrategy.SNAKE_CASE)
-class GuildData (
+data class GuildData (
     val guildId: String,
     val stickyMessage: String? = null,
 )
