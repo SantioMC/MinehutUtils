0cb9ed1b67ddf4032aae419ec538322c5e24278e
tarna <46247728+tarna@users.noreply.github.com>
2025-07-31 22:03:36 -0400

    feat: sticky marketplace message

--- src/main/kotlin/me/santio/minehututils/marketplace/MarketplaceListener.kt
+++ src/main/kotlin/me/santio/minehututils/marketplace/MarketplaceListener.kt
@@ -2,15 +2,46 @@ package me.santio.minehututils.marketplace
 
 import kotlinx.coroutines.launch
 import me.santio.minehututils.bot
+import me.santio.minehututils.cooldown.Cooldown
+import me.santio.minehututils.cooldown.CooldownManager
+import me.santio.minehututils.database.DatabaseHandler
+import me.santio.minehututils.factories.EmbedFactory
 import me.santio.minehututils.logger.GuildLogger
+import me.santio.minehututils.resolvers.DurationResolver.discord
 import me.santio.minehututils.scope
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
 import net.dv8tion.jda.api.events.message.MessageBulkDeleteEvent
 import net.dv8tion.jda.api.events.message.MessageDeleteEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.utils.FileUpload
 
 object MarketplaceListener : ListenerAdapter() {
+
+    override fun onButtonInteraction(event: ButtonInteractionEvent) {
+        if (!event.isFromGuild) return
+
+        scope.launch {
+            val settings = DatabaseHandler.getSettings(event.guild!!.id)
+
+            // minehut:marketplace:post:<type>
+            val type = event.componentId.substringAfter("minehut:marketplace:post:")
+            if (type != "offer" && type != "request") return@launch
+
+            val cooldown = CooldownManager.get(event.user.id, Cooldown.getMarketplaceType(type))
+            if (cooldown != null) {
+                event.replyEmbeds(
+                    EmbedFactory.error(
+                        "You are currently on cooldown, try again ${cooldown.timeLeft().discord(true)}",
+                        event.guild!!
+                    ).build()
+                ).setEphemeral(true).queue()
+                return@launch
+            }
+
+            MarketplaceManager.handlePosting(event, type, settings)
+        }
+    }
 
     override fun onMessageDelete(event: MessageDeleteEvent) {
         if (!event.isFromGuild) return
